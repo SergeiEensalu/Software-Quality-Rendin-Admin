8a710fcc804861faf9e8d92c3b7c9b93498e4a60
vvk <>
2020-10-22 00:52:23 +0300

    setup jest; install  enzyme; first empty test

---
+++ src/tests/Header.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import Header from '../../src/components/Header';
+
+test('test Header component', () => {
+
+    // test('header test', () => {
+    //     const wrapper = mount(<Header />);
+    //     expect(wrapper).toContainHTML;
+    // });
+});
