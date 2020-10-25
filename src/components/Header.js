a596bc9b64ef25e05dd473dc1ffe11ea35251d36
vvk <>
2020-10-25 18:52:06 +0200

    header fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,16 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const Header = () => {
     return (
+        <Router>
         <nav className="main-nav">
             <ul>
                 <li><NavLink to="/payment">Payment</NavLink></li>
                 <li><NavLink to="/settings">Settings</NavLink></li>
             </ul>
-        </nav>    );
+        </nav> </Router>   );
 };
 
 export default Header;
